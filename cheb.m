2d331932a28649ae25429bf9930d6868169ce9c1
Zhentao-He <hzt2183610310@stu.xjtu.edu.cn>
2026-07-07 15:39:19 +0800

    Update cheb.m

--- cheb.m
+++ cheb.m
@@ -1,10 +1,16 @@
-function [D,x]=cheb(N)
+function [D,x] = cheb(N,x1,x2)
 % CHEB compute D = differentiation matrix, x = Chebyshev grid
 if N==0,D=0;x=1;return,end
 
-x=cos(pi*(0:N)/N)';
-c=[2;ones(N-1,1);2].*(-1).^(0:N)';
-X=repmat(x,1,N+1);
-dX=X-X';
-D=(c*(1./c)')./(dX+(eye(N+1)));
-D=D-diag(sum(D,2));
+x = cos(pi*(0:N)/N)'; % [-1,1]
+c = [2;ones(N-1,1);2].*(-1).^(0:N)';
+X = repmat(x,1,N+1);
+dX = X-X';
+D = (c*(1./c)')./(dX+(eye(N+1)));
+D = D-diag(sum(D,2));
+
+if nargin == 3
+    L = x2-x1;
+    x = L/2*(x+1) + x1;
+    D = D/(L/2);
+end
